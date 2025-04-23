de09afe7f38fcc1cefbced83fcc7f1645aec401d
Victory2208 <victorstaussi@yahoo.com>
2025-04-23 10:48:55 +0100

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,21 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBHNiV1lL2oxlNmGKEfJ7qDy7CHshVlM7M",
   authDomain: "idas-kitchen.firebaseapp.com",
+  databaseURL: "https://idas-kitchen-default-rtdb.firebaseio.com",
   projectId: "idas-kitchen",
-  storageBucket: "idas-kitchen.appspot.com",
+  storageBucket: "idas-kitchen.firebasestorage.app",
   messagingSenderId: "177143028894",
   appId: "1:177143028894:web:4823742d52d4cb5d5d6f8f",
+  measurementId: "G-60LM33KNJC",
 };
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export { auth, db };
+
