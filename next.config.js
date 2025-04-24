cc373fa0549b894c36f8cd983bd4f88f88dfa763
Victory2208 <victorstaussi@yahoo.com>
2025-04-24 18:45:34 +0100

    Fix: configure alias for @ path resolution

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+      config.resolve.alias['@'] = require('path').resolve(__dirname, 'src');
+      return config;
+    }
+  };
+  
+  module.exports = nextConfig;
+  
